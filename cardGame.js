869eb9e94b1dd628ef1f9e95f2f6bead7d331145
Regeditted <36388273+NijTeck@users.noreply.github.com>
2020-07-04 17:37:59 -0500

    Create cardGame.js

---
+++ cardGame.js
@@ -0,0 +1,37 @@
+
+var count = 0;
+function cc(card) {
+  switch(card) {
+    case 2:
+
+    case 3: 
+
+    case 4:
+
+    case 5: 
+
+    case 6:
+
+      count++;
+      break;
+
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  var holdBet = "Hold";
+  if(count > 0) { 
+    holdBet = 'Bet'
+
+  }
+  return count + "" + holdBet;
+
+
+}
+
+cc(4); cc('K'); cc(10); cc('K'); cc('A');
+console.log(cc(4))
